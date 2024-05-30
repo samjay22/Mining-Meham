ee76780ddd6447c283a2ae295799538cc9dd6934
samdev22 <samjtaylor22@gmail.com>
2024-05-29 21:40:47 -0500

    Create Entity Changes

--- src/ServerScriptService/Startup/CreateEntityChannels.server.lua
+++ src/ServerScriptService/Startup/CreateEntityChannels.server.lua
@@ -1,7 +1,12 @@
+--!strict
 local Channel = require(game.ReplicatedStorage.Utility.Channel)
 
 for _,v in next, game.ServerStorage.ServerChannels:GetChildren() do
-    local data = require(v)
+    if not v:IsA("ModuleScript") then
+        continue
+    end
+
+    local data = require(v ::  ModuleScript)
     
     for i,d in next, data do
         Channel.New(i, d)
